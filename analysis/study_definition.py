49b3e7ed2ab9d78b98c71f7f6e92cb46c808b850
RachelS99 <87368118+RachelS99@users.noreply.github.com>
2021-11-03 15:31:50 +0000

    Work on study_definition

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -1,13 +1,24 @@
-from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv  # NOQA
+from cohortextractor import (
+    StudyDefinition,
+    Measure,
+    patients
+)
+from codelists import *
 
+study=StudyDefinition(
 
-study = StudyDefinition(
+    index_date="2021-10-01",
+
+    # Configure the expectations framework
     default_expectations={
-        "date": {"earliest": "1900-01-01", "latest": "today"},
-        "rate": "uniform",
+        "date": {"earliest": "2016-03-01", "latest": "today"},
+        "rate": "exponential_increase",
         "incidence": 0.5,
     },
-    population=patients.registered_with_one_practice_between(
-        "2019-02-01", "2020-02-01"
+    population=patients.with_these_medications(
+        doac_codes,
+        between=["2016-03-01", "2021-10-31"],
+        return_expectations={
+            "incidence": 0.9}, 
     ),
 )
