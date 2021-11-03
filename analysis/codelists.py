8df3d27952a77e907f334e3b74605d764089a9a9
RachelS99 <87368118+RachelS99@users.noreply.github.com>
2021-11-03 15:31:28 +0000

    Create codelists.py

---
+++ analysis/codelists.py
@@ -0,0 +1,9 @@
+from cohortextractor import (
+    codelist_from_csv,
+)
+
+doac_codes = codelist_from_csv(
+    "codelists/opensafely-direct-acting-oral-anticoagulants-doac.csv",
+    system="snomed",
+    column='id',
+)
