e35c224a41bca5e770e91acba03f86573245b62a
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2022-02-14 09:32:39 +0000

    test all dose calculation for 1 month

--- analysis/calculate_dose_scaled_back.py
+++ analysis/calculate_dose_scaled_back.py
@@ -50,73 +50,73 @@ for file in os.listdir(OUTPUT_DIR):
                 json.dump(unique_crcl, fp)
 
 
-        # apixaban
-        apixaban_conditions = [
-            ((df["atrial_fib"] == 1) & (df["crcl"] < 15) & (df["crcl"] >= 0)),
-            ((df["atrial_fib"] == 1) & (df["crcl"] >= 15) & (df["crcl"] <= 29)),
-            ((df["atrial_fib"] == 1) & (df["crcl"] >= 30)),
-        ]
+            # apixaban
+            apixaban_conditions = [
+                ((df["atrial_fib"] == 1) & (df["crcl"] < 15) & (df["crcl"] >= 0)),
+                ((df["atrial_fib"] == 1) & (df["crcl"] >= 15) & (df["crcl"] <= 29)),
+                ((df["atrial_fib"] == 1) & (df["crcl"] >= 30)),
+            ]
 
-        apixaban_values = ["nr", "A2.5", "A5"]
+            apixaban_values = ["nr", "A2.5", "A5"]
 
-        df["apixaban"] = np.select(apixaban_conditions, apixaban_values)
+            df["apixaban"] = np.select(apixaban_conditions, apixaban_values)
 
-        # rivaroxaban
-        rivaroxaban_conditions = [
-            ((df["atrial_fib"] == 1) & (df["crcl"] < 15) & (df["crcl"] >= 0)),
-            ((df["atrial_fib"] == 1) & (df["crcl"] >= 15) & (df["crcl"] <= 49)),
-            ((df["atrial_fib"] == 1) & (df["crcl"] > 50)),
-        ]
+            # rivaroxaban
+            rivaroxaban_conditions = [
+                ((df["atrial_fib"] == 1) & (df["crcl"] < 15) & (df["crcl"] >= 0)),
+                ((df["atrial_fib"] == 1) & (df["crcl"] >= 15) & (df["crcl"] <= 49)),
+                ((df["atrial_fib"] == 1) & (df["crcl"] > 50)),
+            ]
 
-        rivaroxaban_values = ["nr", "R15", "R20"]
+            rivaroxaban_values = ["nr", "R15", "R20"]
 
-        df["rivaroxaban"] = np.select(rivaroxaban_conditions, rivaroxaban_values)
+            df["rivaroxaban"] = np.select(rivaroxaban_conditions, rivaroxaban_values)
 
-        # edoxaban
-        edoxaban_conditions = [
-            ((df["atrial_fib"] == 1) & (df["crcl"] < 15) & (df["crcl"] >= 0)),
-            ((df["atrial_fib"] == 1) & (df["crcl"] >= 15) & (df["crcl"] <= 50)),
-            ((df["atrial_fib"] == 1) & (df["crcl"] > 50)),
-        ]
+            # edoxaban
+            edoxaban_conditions = [
+                ((df["atrial_fib"] == 1) & (df["crcl"] < 15) & (df["crcl"] >= 0)),
+                ((df["atrial_fib"] == 1) & (df["crcl"] >= 15) & (df["crcl"] <= 50)),
+                ((df["atrial_fib"] == 1) & (df["crcl"] > 50)),
+            ]
 
-        edoxaban_values = ["nr", "E30", "E60"]
+            edoxaban_values = ["nr", "E30", "E60"]
 
-        df["edoxaban"] = np.select(edoxaban_conditions, edoxaban_values)
+            df["edoxaban"] = np.select(edoxaban_conditions, edoxaban_values)
 
-        # dabigatran
-        dabigatran_conditions = [
-            ((df["atrial_fib"] == 1) & (df["crcl"] < 30) & (df["crcl"] >= 0)),
-            ((df["atrial_fib"] == 1) & (df["crcl"] >= 30) & (df["crcl"] <= 50)),
-            ((df["atrial_fib"] == 1) & (df["crcl"] >= 30)),
-        ]
+            # dabigatran
+            dabigatran_conditions = [
+                ((df["atrial_fib"] == 1) & (df["crcl"] < 30) & (df["crcl"] >= 0)),
+                ((df["atrial_fib"] == 1) & (df["crcl"] >= 30) & (df["crcl"] <= 50)),
+                ((df["atrial_fib"] == 1) & (df["crcl"] >= 30)),
+            ]
 
-        dabigatran_values = ["nr", "D110", "D150"]
+            dabigatran_values = ["nr", "D110", "D150"]
 
-        df["dabigatran"] = np.select(dabigatran_conditions, dabigatran_values)
-       
-        # now need to check if on correct dose!
-        conditions = [
-            (df["doac_dose_calculated"] == df["apixaban"]),
-            (df["doac_dose_calculated"] == df["rivaroxaban"]),
-            (df["doac_dose_calculated"] == df["edoxaban"]),
-            (df["doac_dose_calculated"] == df["dabigatran"]),
-        ]
+            df["dabigatran"] = np.select(dabigatran_conditions, dabigatran_values)
+        
+            # now need to check if on correct dose!
+            conditions = [
+                (df["doac_dose_calculated"] == df["apixaban"]),
+                (df["doac_dose_calculated"] == df["rivaroxaban"]),
+                (df["doac_dose_calculated"] == df["edoxaban"]),
+                (df["doac_dose_calculated"] == df["dabigatran"]),
+            ]
 
-        values = ["1", "1", "1", "1"]
+            values = ["1", "1", "1", "1"]
 
-        df["dose_match"] = np.select(conditions, values)
+            df["dose_match"] = np.select(conditions, values)
 
-        # with af & crcl recorded & exclusions
-        afcrcl_conditions = [
-            ((df["atrial_fib"] == 1) & (df["crcl_recorded"] == 1)),
-            ((df["atrial_fib"] == 0)),
-            ((df["crcl_recorded"] == 0)),
-            (df["crcl_exclude"] == 0),
-        ]
+            # with af & crcl recorded & exclusions
+            afcrcl_conditions = [
+                ((df["atrial_fib"] == 1) & (df["crcl_recorded"] == 1)),
+                ((df["atrial_fib"] == 0)),
+                ((df["crcl_recorded"] == 0)),
+                (df["crcl_exclude"] == 0),
+            ]
 
-        afcrcl_values = ["1", "0", "0", "0"]
+            afcrcl_values = ["1", "0", "0", "0"]
 
-        df["af_&_crcl"] = np.select(afcrcl_conditions, afcrcl_values)
+            df["af_&_crcl"] = np.select(afcrcl_conditions, afcrcl_values)
 
-        # df.to_csv(f'output/df_with_calculation_{date}.csv') # this will be a new file
-        df.to_csv(os.path.join(OUTPUT_DIR, file))  # this will overwrite
+            # df.to_csv(f'output/df_with_calculation_{date}.csv') # this will be a new file
+            df.to_csv(os.path.join(OUTPUT_DIR, file))  # this will overwrite
